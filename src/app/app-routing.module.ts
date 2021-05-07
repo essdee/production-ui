070c5748cb71d4455dfef4bcf69ee8cd9b7b1dc6
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-07 16:54:14 +0530

    feat: routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth-screens/login/login.component';
+import { OtpComponent } from './modules/auth-screens/otp/otp.component';
+import { SetPasswordComponent } from './modules/auth-screens/set-password/set-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:LoginComponent},
+  { path: 'otp', component: OtpComponent},
+  { path: 'set-password', component: SetPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
