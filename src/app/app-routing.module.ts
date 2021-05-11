fb99b2cb7cae4cd70ddca8bebfab29d42752367f
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-11 10:30:30 +0530

    feat: spinner code updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth-screens/login/login.component';
 import { OtpComponent } from './modules/auth-screens/otp/otp.component';
 import { SetPasswordComponent } from './modules/auth-screens/set-password/set-password.component';
+import { HomeScreenComponent } from './modules/home-screen/home-screen.component';
 
 const routes: Routes = [
-  { path: '', component:LoginComponent},
+  { path: '', component: LoginComponent},
   { path: 'otp', component: OtpComponent},
-  { path: 'set-password', component: SetPasswordComponent}
+  { path: 'set-password', component: SetPasswordComponent},
+  { path: 'home', component: HomeScreenComponent}
 ];
 
 @NgModule({
