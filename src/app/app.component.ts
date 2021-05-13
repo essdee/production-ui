77bfae2a06b4f776813d7903e8cca9993038d0a3
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-13 16:45:44 +0530

    feat: toast component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { ToastService } from './services/toast/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'production-ui';
+  constructor(
+    public toastService: ToastService) {}
+  showSuccess(successTpl: string | TemplateRef<any>) {
+    this.toastService.show(successTpl, {
+      classname: 'bg-success text-light',
+      delay: 2000 ,
+      autohide: true
+    });
+  }
+
+  showDanger(dangerTpl: string | TemplateRef<any>) {
+    this.toastService.show(dangerTpl, { classname: 'bg-danger text-light', delay: 5000 });
+  }
 }
