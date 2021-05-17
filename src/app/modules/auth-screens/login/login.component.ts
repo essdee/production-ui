e84c002e5d65368010a7404bdc0e3f35cf6cb0a8
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-17 15:25:29 +0530

    fix: updatelogin component api's

--- src/app/modules/auth-screens/login/login.component.ts
+++ src/app/modules/auth-screens/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { AppComponent } from 'src/app/app.component';
 import { ToastService } from 'src/app/services/toast/toast.service';
 import { UserManagementApiService } from 'src/app/services/user-management-api/user-management-api.service';
@@ -10,12 +11,13 @@ import { UserManagementApiService } from 'src/app/services/user-management-api/u
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  mobileNumber!: string;
-  password!: string;
+  mobileNumber: any;
+  password: any;
   isLoginClicked = false;
-  isverified!: boolean;
+  isVerified!: boolean;
 
   constructor(private router: Router, public toastService: ToastService, public userManagementApi: UserManagementApiService,
+    private cookieService: CookieService,
     public appComponent: AppComponent) {}
 
   ngOnInit(): void {
@@ -26,31 +28,33 @@ export class LoginComponent implements OnInit {
       return;
     }
     this.isLoginClicked = true;
-    await this.userManagementApi.verifyUser(this.mobileNumber, this.password)
-    // console.log(this.userManagementApi.result);
+    const body = {
+      "user": this.mobileNumber,
+      "pwd": this.password
+      };
 
-    // Todo: Need to handle response here
-  //   if(this.userManagementApi.result){
-  //     if(this.userManagementApi.result.message.status == 'Failed'){
-  //     this.isLoginClicked = false;
-  //     this.appComponent.showDanger('Invalid Password!');
-  //   }
-  //   if(this.userManagementApi.result.message.status == 'Success'){
-  //     this.showHomeScreen();
-  //   }
-  //   if(this.userManagementApi.result.message.error_message == "User not found"){
-  //     this.isLoginClicked = false;
-  //     this.appComponent.showDanger('User not Found');
-  //   }
-  // }
-  // else{
-  //   this.isLoginClicked = false;
-  //   this.appComponent.showDanger('Invalid Password!');
-  // }
+    try {
+      const res = await this.userManagementApi.api_call('post', 'login',body)
+      if (res){
+        this.cookieService.set( 'userName', res['full_name'] ); 
+        this.showHomeScreen();
+      }
+    } catch(error) {
+       if(error.status == 401){
+        this.isLoginClicked = false;
+        this.appComponent.showDanger('Invalid mobile number or password!');
+       }
+       else{
+        this.isLoginClicked = false;
+        this.mobileNumber = null;
+        this.password = null;
+        this.appComponent.showDanger('Something went wrong please try again!');
+       }
+    }
   }
   showHomeScreen(){
-    this.isverified = true;
-    if(this.isverified){
+    this.isVerified = true;
+    if(this.isVerified){
     this.router.navigateByUrl('/home');
     }
   }
