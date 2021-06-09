b298a5099a34f6712c110da713373912bbd0ec0a
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-06-09 11:27:47 +0530

    update: packages

--- src/app/modules/master/supplier/supplier/supplier.component.ts
+++ src/app/modules/master/supplier/supplier/supplier.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IDropdownSettings} from 'ng-multiselect-dropdown';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
